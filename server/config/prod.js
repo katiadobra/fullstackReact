6a8cf5238f2e17481a5372d07273abb55dfc8263
Kat Dobra <broun.kat@gmail.com>
2017-12-27 18:07:49 +0200

    adding create-react-app; refactoring in passport.js with AsyncAwait

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,6 +3,5 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  googleRedirectURI: 'https://bayou.herokuapp.com'
+  cookieKey: process.env.COOKIE_KEY
 };
